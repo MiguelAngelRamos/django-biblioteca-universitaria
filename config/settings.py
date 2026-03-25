8e099c8c3c85300bbfc159cc460c68645b78e540
Miguel Ramos <return.infox@gmail.com>
2026-03-24 21:38:59 -0300

    admin

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
- 
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Editorial, Estudiante, Credencial, Libro, Prestamo
 # Register your models here.
+
+@admin.register(Estudiante)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ('matricula', 'nombre_completo')
+    search_fields = ('matricula', 'nombre_completo')
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editorial', 'stock_disponible')
+    list_filter = ('editorial',)
+    search_fields = ('titulo',)
+
+@admin.register(Prestamo)
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = ('estudiante', 'libro', 'fecha_prestamo', 'estado', 'multa_generada')
+    list_filter = ('estado', 'fecha_prestamo')
+
+admin.site.register(Editorial)
+admin.site.register(Credencial)
