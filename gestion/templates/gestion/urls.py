f7360dec86b234ad3888d0021926878da6aa8023
Miguel Ramos <return.infox@gmail.com>
2026-03-25 22:07:31 -0300

    proyecto terminado

--- gestion/templates/gestion/urls.py
+++ gestion/templates/gestion/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 1. ORM Standard Vistas Base
+    # 1. ORM Standard Vistas
     path('', views.LibroListView.as_view(), name='libro_list'),
     path('estudiantes/', views.EstudianteListView.as_view(), name='estudiante_list'),
     path('estudiantes/nuevo/', views.EstudianteCreateView.as_view(), name='estudiante_create'),
@@ -10,8 +10,8 @@ urlpatterns = [
     path('editoriales/nueva/', views.EditorialCreateView.as_view(), name='editorial_create'),
     
     # 2. Consultas Personalizadas / Raw SQL
-    path('reporte-avanzado/', views.ReporteSQLCrudoView.as_view(), name='reporte_avanzado'),
+    path('reporte-avanzado/', views.reporte_sql_crudo, name='reporte_avanzado'),
     
-    # 3. Invocación segura de SP's y Cursores
-    path('ejecutar-sp/<int:estudiante_id>/', views.EjecutarProcedimientoView.as_view(), name='ejecutar_sp'),
+    # 3. Invocación de Procedimientos Almacenados y Cursores
+    path('ejecutar-sp/<int:estudiante_id>/', views.ejecutar_procedimiento_cursor, name='ejecutar_sp'),
 ]
